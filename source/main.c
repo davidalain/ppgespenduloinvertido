cf0e9dd2d0d43f77bfc9fbc9e6460c3ed0f29ad7
barboza.erick <barboza.erick@17a6feb8-f218-85d2-6042-6051b4057b28>
2012-05-15 01:29:49 +0000

    Dei um merge com o código de David. O código está funcionando, porém so pode receber 3 bytes por vez pela serial. Além disso, a função de converção de String para inteiro (StringToInt) está retornando um número negativo para o valor do tempo_ang.

--- source/main.c
+++ source/main.c
@@ -4,6 +4,7 @@
 #fuses HS, NOWDT, NOPROTECT, NOPUT, NOBROWNOUT, NOLVP
 #use rs232 (baud = 9600, bits = 8, stream=PEND, uart1, errors) //configurações para a porta serial xmit = pin_c6, rcv = pin_c7
 
+#define BUFFER_SIZE 20
 
 typedef struct packet
 {
@@ -15,35 +16,125 @@ typedef struct packet
    int estado_pendulo;
 }ReceivePacket;
 
+typedef enum{
+   ESPERANDO_H,
+   ESPERANDO_POS,
+   ESPERANDO_TEMPO_POS,
+   ESPERANDO_ANG,
+   ESPERANDO_TEMPO_ANG,
+   ESPERANDO_ESTADO_CARRO,
+   ESPERANDO_ESTADO_PENDULO,
+   ESPERANDO_BARRA_N
+}EstadoRecepcao;
+
+int StringToInt(char* string, int size);
+void decode();
+
+
 BOOLEAN recebeuTudo;
 int indexString;
-char string_recebida[50];
+char string_recebida[BUFFER_SIZE];
 ReceivePacket pacote;
+
+char numeroRecebido[3];
+int indiceNumeroRecebido;
+EstadoRecepcao estadoAtual = ESPERANDO_H;
+
 
 #int_rda
 void reception ()
 {
-  char temp;
-  temp = getch();
+   
+   char temp;
+   temp = getch();
+   
+   string_recebida[ indexString++ ] = temp; 
+   
+   printf(" i=%d c=%c \n", (indexString-1), temp);
   
-  if(temp != '\n')
-  {
-   printf("\ti:%d c:%c |", indexString,temp);
-   string_recebida[indexString++] = temp;
-  }
-  else
-  {
-   recebeuTudo = TRUE;
-  }
+   if(temp == '\n'){
+      recebeuTudo = TRUE;
+   }
+   
+}
+
+
+//O protocolo é: H,<pos_carro>,<tempo_carro>,<angulo>,<tempo_angulo>,<estado_carro>,<estado_pendulo>,\n 
+
+void decode(){
+
+   char i, temp;
+   for(i = 0 ; i < indexString ; i++){
+      temp = string_recebida[i];
+      
+      if(estadoAtual == ESPERANDO_H){
+         if(temp == ','){
+            estadoAtual = ESPERANDO_POS;
+         }
+      }else if(estadoAtual == ESPERANDO_POS){
+         if(temp == ','){
+            pacote.posicao = StringToInt(numeroRecebido, indiceNumeroRecebido);
+            indiceNumeroRecebido = 0;
+            estadoAtual = ESPERANDO_TEMPO_POS;
+         }else{
+            numeroRecebido[ indiceNumeroRecebido++ ] = temp;
+         }
+      }else if(estadoAtual == ESPERANDO_TEMPO_POS){
+         if(temp == ','){
+            pacote.tempo_pos = StringToInt(numeroRecebido, indiceNumeroRecebido);
+            indiceNumeroRecebido = 0;
+            estadoAtual = ESPERANDO_ANG;
+         }else{
+            numeroRecebido[ indiceNumeroRecebido++ ] = temp;
+         }
+      }else if(estadoAtual == ESPERANDO_ANG){
+         if(temp == ','){
+            pacote.angulo = StringToInt(numeroRecebido, indiceNumeroRecebido);
+            indiceNumeroRecebido = 0;
+            estadoAtual = ESPERANDO_TEMPO_ANG;
+         }else{
+            numeroRecebido[ indiceNumeroRecebido++ ] = temp;
+         }
+      }else if(estadoAtual == ESPERANDO_TEMPO_ANG){
+         if(temp == ','){
+            pacote.tempo_ang = StringToInt(numeroRecebido, indiceNumeroRecebido);
+            indiceNumeroRecebido = 0;
+            estadoAtual = ESPERANDO_ESTADO_CARRO;
+         }else{
+            numeroRecebido[ indiceNumeroRecebido++ ] = temp;
+         }
+      }else if(estadoAtual == ESPERANDO_ESTADO_CARRO){
+         if(temp == ','){
+            pacote.estado_carro = StringToInt(numeroRecebido, indiceNumeroRecebido);
+            indiceNumeroRecebido = 0;
+            estadoAtual = ESPERANDO_ESTADO_PENDULO;
+         }else{
+            numeroRecebido[ indiceNumeroRecebido++ ] = temp;
+         }
+      }else if(estadoAtual == ESPERANDO_ESTADO_PENDULO){
+         if(temp == ','){
+            pacote.estado_pendulo = StringToInt(numeroRecebido, indiceNumeroRecebido);
+            indiceNumeroRecebido = 0;
+            estadoAtual = ESPERANDO_BARRA_N;
+         }else{
+            numeroRecebido[ indiceNumeroRecebido++ ] = temp;
+         }
+      }else if(estadoAtual == ESPERANDO_BARRA_N){
+         if(temp == '\n'){
+            recebeuTudo = TRUE;
+            estadoAtual = ESPERANDO_H;
+         }
+      }
+   }
+   
 }
 
 //Assumindo string nnn
 int StringToInt(char* string, int size)
 {   
    char offset = '0';
+   int i;
    int value = 0;
-   
-    printf("E ");
    
    if(size == 3)
    {
@@ -60,58 +151,29 @@ int StringToInt(char* string, int size)
    {
       value += (string[0]-offset);
    }
+
+   printf("string : ");
+   for(i=0; i<size; i++)
+   {
+      printf("%c ", string[i]);
+   }
    
+   if(value < 0)
+   {
+		printf("\noffset = %d ", offset);
+   }
+   
+   printf("\nvalue = %d \n", value);
    return value;
 }
 
-/*
-*  Retorna um char[] com ["posicao", "tempo_pos", "angulo", "tempo_angulo", "estado_carro", "estado_pendulo"]
-*/
-char* SplitPacket(char* source)
-{
-   
-   char* term, ptr, retorno[6];
-   int i;
-   
-    printf("C ");
-   
-   term = ",";
-   ptr = strtok(source, term); // Inicio do pacote "H"
-   
-   for(i=0; i<6; i++)
-   {
-      ptr = strtok(0, term);
-      retorno[i] = *ptr;
-   }
-   
-   return retorno;
-}
 
-void processPacket ()
-{
-   char dados[6];
-   
-   printf("B ");
-   
-   *dados = SplitPacket(string_recebida);
-   
-    printf("D ");
-    
-   pacote.posicao = StringToInt(dados[0], 3);
-   pacote.tempo_pos = StringToInt(dados[1], 3);
-   pacote.angulo = StringToInt(dados[2], 3);
-   pacote.tempo_ang = StringToInt(dados[3], 3);
-   pacote.estado_carro = StringToInt(dados[4], 3);
-   pacote.estado_pendulo = StringToInt(dados[5], 3);
-   
-    printf("F ");
-}
 
 void main ()
 {
    enable_interrupts(global);     //habilita a interrupção global
    enable_interrupts(int_rda);    //habilita a interrupção de recepção de caractere pela porta serial
-   
+
    pacote.posicao = -1;
    pacote.tempo_pos = -1;
    pacote.angulo = -1;
@@ -120,24 +182,22 @@ void main ()
    pacote.estado_pendulo = -1;
    
    while (true)            //loop de repetição do código principal
-   {
+   {   
+      
       if(recebeuTudo)
       {
-         printf(" A ");
-        recebeuTudo = FALSE;
-        processPacket();
-      }
-   
-      //if(pacote.posicao >= 0)
-      //{
+         decode();
+         
          printf("%d, ", pacote.posicao);
          printf("%d, ", pacote.tempo_pos);
          printf("%d, ", pacote.angulo);
          printf("%d, ", pacote.tempo_ang);
          printf("%d, ", pacote.estado_carro);
          printf("%d\n", pacote.estado_pendulo);
-      //}
+
+         recebeuTudo = FALSE;
+       indexString = 0;
+      }
       
-      delay_ms (1000);
    }   // fim do while
 }      // fim do main
